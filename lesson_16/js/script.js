d79eaede3db5c4cc29f9857fb24c2de13a17c044
sofiiaruban <sophiaruban249324@gmail.com>
2025-11-30 17:03:27 +0200

    feat: Add solution

---
+++ lesson_16/js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+document.addEventListener('click', documentActions)
+
+function documentActions(e) {
+  const targetElement = e.target
+
+  if (targetElement.closest('.icon-menu')) {
+    document.documentElement.toggleAttribute('data-menu-open')
+  }
+}
